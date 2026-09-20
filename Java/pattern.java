8e5a22da7dc3348d4b68edf8996823cf48896a4e
SUSHANT3838 <sushantshete310@gmail.com>
2026-09-21 05:27:20 +0530

    Pattern

--- Java/pattern.java
+++ Java/pattern.java
@@ -1,8 +1,16 @@
+import java.util.*;
+
 public class pattern {
-    public static void maiin(String[] args){
-        for(int i=0; i < 4; i++){
-            System.out.print("*");
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
             System.out.println();
         }
+        sc.close();
     }
 }
