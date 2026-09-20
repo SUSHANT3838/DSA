ed2abd67d3894b4688c5f9d6d8f384445430b9b9
SUSHANT3838 <sushantshete310@gmail.com>
2026-09-21 05:10:50 +0530

    DSA Java

--- Java/base.java
+++ Java/base.java
@@ -1,5 +1,9 @@
 public class base {
+    // greeting function
+    public static void greet(String x){
+        System.out.println("Hello, " +  x);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        greet("Sushant");
     }
 }
---
+++ Java/pattern.java
@@ -0,0 +1,8 @@
+public class pattern {
+    public static void maiin(String[] args){
+        for(int i=0; i < 4; i++){
+            System.out.print("*");
+            System.out.println();
+        }
+    }
+}
