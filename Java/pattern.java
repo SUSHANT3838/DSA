929ef78df9c0373c41df780eb68a375bc01b0be7
SUSHANT3838 <sushantshete310@gmail.com>
2026-09-21 07:41:40 +0530

    Update

--- Java/pattern.java
+++ Java/pattern.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
 public class pattern {
-
-    public static void Display(){
+    // For loop
+    public static void Display_1(){
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("=== Using For Loop ===");
         System.out.print("Enter the number : ");
         int n = sc.nextInt();
         for(int i=1; i<=n; i++){
@@ -15,7 +15,26 @@ public class pattern {
         }
         sc.close();
     }
+    // while loop
+    public static void Display_2(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("=== Using While Loop ===");
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        int i = 1;
+        while(i < n){
+            int j = 1;
+            while(j < i + 1){
+                System.out.print("* ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+        sc.close();
+    }
     public static void main(String[] args){
-        Display();
+        //Display_1();// for looe
+        Display_2();// while loop
     }
 }
